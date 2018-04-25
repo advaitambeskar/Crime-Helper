0bfb9d59bfd3acd27bcfad6ef6d911b5eab009f4
abhinavdas1 <shubham.swapnil94@gmail.com>
2018-04-25 17:38:53 -0400

    Unsafe Areas Get Added

--- routes.js
+++ routes.js
@@ -1,11 +1,15 @@
 import {getUsers} from './app/Controllers/userController.js';
+import {getUnsafeAreas} from './app/Controllers/crimeController.js';
 var bodyParser = require('body-parser');
 
 module.exports = function(app) {
 
   app.get('/api/v1/getAllUsers', function(req, res){
     var rows = getUsers(req, res);
-    console.log(rows);
+  });
+
+  app.get('/api/v1/getUnsafeAreas', function(req, res){
+    var rows = getUnsafeAreas(req, res);
     
   });
 };
